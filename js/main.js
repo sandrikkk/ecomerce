6dd5108ba4d16beacbc7e0502926bbab40991b75
giorgi-maker <73039625+giorgi-maker@users.noreply.github.com>
2022-05-03 12:11:30 +0400

    responsive navigation

--- js/main.js
+++ js/main.js
@@ -1,3 +1,14 @@
+const menuBtn= document.querySelector(".menu-icon span");
+const searchBtn= document.querySelector(".search-icon");
+const cancelBtn= document.querySelector(".cancel-icon");
+const items= document.querySelector(".nav-items");
+const form =document.querySelector("form")
+
+menuBtn.onClick = () =>{
+  items.classList.add("active");
+}
+
+//swiper
 const swiper = new Swiper('.swiper', {
     autoplay:{
         delay:3000,
